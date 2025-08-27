672be5f1cfd206f7f4c9cbef2dfc57d7b17c1b18
Wardo30 <jowardremeticado30@gmail.com>
2025-08-27 15:34:11 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXAmDbT0dbw91GFP_249EBy1WyYNOzznI",
+  authDomain: "snby-goals.firebaseapp.com",
+  projectId: "snby-goals",
+  storageBucket: "snby-goals.firebasestorage.app",
+  messagingSenderId: "503875843314",
+  appId: "1:503875843314:web:1598c20a00c3f52557cc6b"
+};
+
+const app = initializeApp(firebaseConfig);
